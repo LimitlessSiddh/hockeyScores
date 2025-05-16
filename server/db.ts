facb1e74b55ecb3c72f26d49e5695c9bbbce2ea9
LimitlessSiddh <siddhpat4@gmail.com>
2025-05-16 17:42:34 -0400

    db

--- server/db.ts
+++ server/db.ts
@@ -6,6 +6,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // ✅ Required for Render and most hosted PG providers
+  }
 });
 
 export default pool;
